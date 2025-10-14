28530294e3e8312e115a6630fdfaf84ce8d55d7d
nctammhyp <tamnguyen@hypmotion.com>
2025-10-14 11:44:51 +0700

    update

--- trainv1_distill.py
+++ trainv1_distill.py
@@ -168,9 +168,9 @@ def inference_sample(model, state_path, device, model_type="last"):
             # ----- Load Depth từ file .npy -----
             gt_depth = np.load(label_path).astype(np.float32)
 
-            # Resize về input size của model (160x128)
-            rgb_resized = cv2.resize(rgb, (160, 128))
-            gt_resized = cv2.resize(gt_depth, (160, 128))
+            # Resize về input size của model (160x128) 168, 126
+            rgb_resized = cv2.resize(rgb, (168, 126))
+            gt_resized = cv2.resize(gt_depth, (168, 126))
 
             # Normalize GT depth để visualize
             gt_resized = (gt_resized - gt_resized.min()) / (gt_resized.max() - gt_resized.min() + epsilon)
