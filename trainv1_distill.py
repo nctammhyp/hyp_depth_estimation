e89f1ad9ece38270c997efd89a39ac85c5fff533
nctammhyp <tamnguyen@hypmotion.com>
2025-10-14 11:37:57 +0700

    update

--- trainv1_distill.py
+++ trainv1_distill.py
@@ -409,7 +409,7 @@ def trainer():
     device = "cuda:0"
 
     teacher_model = DepthAnythingV2(encoder='vitl', features=256, out_channels=[256, 512, 1024, 1024])
-    teacher_model.load_state_dict(torch.load(r'depth_anything_v2\checkpoint\depth_anything_v2_vitl.pth', map_location='cpu'))
+    teacher_model.load_state_dict(torch.load('/kaggle/working/depth_anything_v2_vitl.pth', map_location='cuda:0'))
 
     # student
     student_model = FastDepthV2()
