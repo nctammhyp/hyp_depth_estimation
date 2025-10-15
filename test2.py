607646a62879a66a2431bb02ceedc3cb927f4bdd
nctammhyp <tamnguyen@hypmotion.com>
2025-10-15 13:44:08 +0700

    update

--- test2.py
+++ test2.py
@@ -1,5 +1,9 @@
-import numpy as np
+import torch
+import torch.nn.functional as F
 
-p = np
+x = torch.tensor([-2.0, -1.0, 0.0, 1.0, 2.0])
+y1 = F.relu(x)
+y2 = F.relu(y1)
 
-print(p == np)
+print(y1)  # tensor([0., 0., 0., 1., 2.])
+print(y2)  # tensor([0., 0., 0., 1., 2.]) → không đổi
