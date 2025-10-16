6572adbdf1c0c04c42ccbebc2a0295f0bedcc576
nctammhyp <tamnguyen@hypmotion.com>
2025-10-16 10:20:20 +0700

    update

--- trainv9.py
+++ trainv9.py
@@ -522,4 +522,4 @@ def train_fn(device = "cuda:0", load_state = False, state_path = './'):
 
 if __name__ == "__main__":
     # train_fn(device='cuda:0', load_state=False, state_path="/kaggle/working/hyp_depth_estimation/ours_checkpoints/16")
-    train_fn(device='cuda:0', load_state=False, state_path="/home/gremsy_guest/hyp_workspace/hyp_depth_estimation/ours_checkpoints/16")
+    train_fn(device='cuda:0', load_state=False, state_path="/home/gremsy_guest/hyp_workspace/hyp_depth_estimation/ours_checkpoints/17")
