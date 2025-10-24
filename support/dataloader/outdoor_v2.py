2c47a054fdd275b8ca3f80c280d8a5dcf89009fe
nctammhyp <tamnguyen@hypmotion.com>
2025-10-24 10:43:54 +0700

    update

--- support/dataloader/outdoor_v2.py
+++ support/dataloader/outdoor_v2.py
@@ -93,8 +93,8 @@ def get_image_label_pairs(directory, img_ext=".jpg", label_ext=".npy"):
     # lbl_root = os.path.join(directory, "labels_npy")
     lbl_root = os.path.join(directory, "labels_npy_322_196")
 
-    print(f"img root: {img_root}")
-    print(f"lbl_root: {lbl_root}")
+    # print(f"img root: {img_root}")
+    # print(f"lbl_root: {lbl_root}")
     
     pairs = []
     if not os.path.exists(img_root) or not os.path.exists(lbl_root):
@@ -186,12 +186,12 @@ def create_data_loaders(data_root, batch_size=64, size=(160, 128)):
 
     train_dir = os.path.join(data_root, "train")
     # print(f"val path: {train_dir}")
-    print(f"val_paths: {train_paths}")
+    # print(f"val_paths: {train_paths}")
 
 
     val_dir = os.path.join(data_root, "val")
     # print(f"val path: {val_dir}")
-    print(f"val_paths: {val_paths}")
+    # print(f"val_paths: {val_paths}")
 
 
 
