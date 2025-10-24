21d9577684be89561f4197ba9d4262e733d66355
nctammhyp <tamnguyen@hypmotion.com>
2025-10-24 14:20:56 +0700

    update

--- support/dataloader/outdoor_v2.py
+++ support/dataloader/outdoor_v2.py
@@ -225,7 +225,7 @@ def create_data_loaders(data_root, batch_size=64, size=(160, 128)):
                               shuffle=True, num_workers=8,
                               pin_memory=True, drop_last=True)
 
-    val_loader = DataLoader(val_dataset, batch_size=1,
+    val_loader = DataLoader(val_dataset, batch_size=batch_size,
                             shuffle=False, num_workers=8,
                             pin_memory=True)
 
