0633bdfb101f3820fdaa5f2ee839a7b7a8807a8e
nctammhyp <tamnguyen@hypmotion.com>
2025-10-24 10:36:06 +0700

    update

--- support/dataloader/outdoor_v2.py
+++ support/dataloader/outdoor_v2.py
@@ -93,8 +93,8 @@ def get_image_label_pairs(directory, img_ext=".png", label_ext=".npy"):
     # lbl_root = os.path.join(directory, "labels_npy")
     lbl_root = os.path.join(directory, "labels_npy_322_196")
 
-    # print(f"img root: {img_root}")
-    # print(f"lbl_root: {lbl_root}")
+    print(f"img root: {img_root}")
+    print(f"lbl_root: {lbl_root}")
     
     pairs = []
     if not os.path.exists(img_root) or not os.path.exists(lbl_root):
