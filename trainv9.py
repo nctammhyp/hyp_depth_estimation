7dd3691abed17ca9adf1ddfbaab92fa916025a87
nctammhyp <tamnguyen@hypmotion.com>
2025-10-28 13:34:06 +0700

    update

--- trainv9.py
+++ trainv9.py
@@ -132,9 +132,9 @@ def eval_depth(pred, target, criterion):
     )
 
     mask = (target >= 1e-3)
-    loss = criterion(pred, target, mask)
+    # loss = criterion(pred, target, mask)
     # loss = criterion(pred, target)
-    # loss = torch.tensor(0.0)
+    loss = torch.tensor(0.0)
 
 
     return {
@@ -582,9 +582,9 @@ def train_fn(device = "cuda:0", load_state = False, state_path = './'):
         }, f"{state_path}/last_checkpoint.pth")
 
         # if results['abs_rel'] < best_val_absrel:
-        if results['loss'] < best_val:
+        if results['rmse'] < best_val:
 
-            best_val = results['loss']
+            best_val = results['rmse']
             new_ckpt = f"{state_path}/best_checkpoint.pth"
 
             # 1. Lưu checkpoint mới
